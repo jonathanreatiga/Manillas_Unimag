c227323e30b47d250065defa27a0b44231dcd9b0
jonathanreatiga <joalrezu2@gmail.com>
2020-05-30 00:24:21 -0500

    Implementacion terminada

--- app/src/main/java/com/example/manillas/MainActivity.java
+++ app/src/main/java/com/example/manillas/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText CANTIDAD;
     private TextView RESULTADO, VALOR;
 
+    private int FILAS = 20, COLUMNAS = 2;
+    private String ARRAY_PRECIOS[][];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         CANTIDAD = findViewById(R.id.lblCantidad);
         RESULTADO = findViewById(R.id.lblResultado);
+        VALOR = findViewById(R.id.txtRValorUnidad);
 
         // MATERIALES
         cmb_MATERIALES = findViewById(R.id.cmbMaterial);
@@ -56,6 +60,58 @@ public class MainActivity extends AppCompatActivity {
         MONEDAS = getResources().getStringArray(R.array.Monedas);
         ADAPTER = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MONEDAS);
         cmb_MONEDA.setAdapter(ADAPTER);
+
+        ARRAY_PRECIOS = new String[FILAS][COLUMNAS];
+
+        // ORO
+        ARRAY_PRECIOS[0][0] = "0;0;0";
+        ARRAY_PRECIOS[0][1] = "100";
+        ARRAY_PRECIOS[1][0] = "0;1;0";
+        ARRAY_PRECIOS[1][1] = "120";
+        ARRAY_PRECIOS[2][0] = "1;0;0";
+        ARRAY_PRECIOS[2][1] = "90";
+        ARRAY_PRECIOS[3][0] = "1;1;0";
+        ARRAY_PRECIOS[3][1] = "110";
+
+        // BAÑO ORO
+        ARRAY_PRECIOS[4][0] = "0;0;1";
+        ARRAY_PRECIOS[4][1] = "100";
+        ARRAY_PRECIOS[5][0] = "0;1;1";
+        ARRAY_PRECIOS[5][1] = "120";
+        ARRAY_PRECIOS[6][0] = "1;0;1";
+        ARRAY_PRECIOS[6][1] = "90";
+        ARRAY_PRECIOS[7][0] = "1;1;1";
+        ARRAY_PRECIOS[7][1] = "110";
+
+        // ORO ROSADO
+        ARRAY_PRECIOS[8][0] = "0;0;2";
+        ARRAY_PRECIOS[8][1] = "100";
+        ARRAY_PRECIOS[9][0] = "0;1;2";
+        ARRAY_PRECIOS[9][1] = "120";
+        ARRAY_PRECIOS[10][0] = "1;0;2";
+        ARRAY_PRECIOS[10][1] = "90";
+        ARRAY_PRECIOS[11][0] = "1;1;2";
+        ARRAY_PRECIOS[11][1] = "110";
+
+        // PLATA
+        ARRAY_PRECIOS[12][0] = "0;0;3";
+        ARRAY_PRECIOS[12][1] = "80";
+        ARRAY_PRECIOS[13][0] = "0;1;3";
+        ARRAY_PRECIOS[13][1] = "100";
+        ARRAY_PRECIOS[14][0] = "1;0;3";
+        ARRAY_PRECIOS[14][1] = "70";
+        ARRAY_PRECIOS[15][0] = "1;1;3";
+        ARRAY_PRECIOS[15][1] = "90";
+
+        // NIQUEL
+        ARRAY_PRECIOS[16][0] = "0;0;4";
+        ARRAY_PRECIOS[16][1] = "70";
+        ARRAY_PRECIOS[17][0] = "0;1;4";
+        ARRAY_PRECIOS[17][1] = "90";
+        ARRAY_PRECIOS[18][0] = "1;0;4";
+        ARRAY_PRECIOS[18][1] = "50";
+        ARRAY_PRECIOS[19][0] = "1;1;4";
+        ARRAY_PRECIOS[19][1] = "80";
     }
 
     public void btnLimpiarCampos(View view) {
@@ -66,114 +122,57 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnCalcular(View view) {
         if (validarCampoNumero()){
-            validarMaterialesYDije();
-
-
+            calcularLeseccionados();
         }
     }
 
-    public void validarMaterialesYDije(){
-        switch (cmb_MATERIALES.getSelectedItemPosition()){
-            case 0:
-                // CUERO
-                switch (cmb_DIJE.getSelectedItemPosition()){
+    public void calcularLeseccionados(){
+        String campo;
+        int calculoPeso=0;
+        int cantidad = Integer.parseInt(CANTIDAD.getText().toString());
+
+       int material = cmb_MATERIALES.getSelectedItemPosition();
+       int dije = cmb_DIJE.getSelectedItemPosition();
+       int tipo = cmb_TIPO.getSelectedItemPosition();
+       int peso = cmb_MONEDA.getSelectedItemPosition();
+
+       campo = Integer.toString(material)+";"+Integer.toString(dije)+";"+Integer.toString(tipo);
+
+        campo = buscarPrecios(campo);
+        if (campo != null){
+            switch (peso){
                 case 0:
-                    // MARTILLO
-                    validarTipoYValor(0, 0);
-                    switch (cmb_TIPO.getSelectedItemPosition()){
-                        case 0:
-                            // OR
-                            break;
-                        case 1:
-                            // Baño de Oro
-                            break;
-                        case 2:
-                            // Oro Rosado
-                            break;
-                        case 3:
-                            // Plata
-                            break;
-                        case 4:
-                            // Níquel
-                            break;
-                    }
+                    calculoPeso = Integer.parseInt(campo);
+                    VALOR.setText(Integer.toString(calculoPeso));
+                    calculoPeso = calculoPeso * cantidad;
+                    RESULTADO.setText("");
+                    RESULTADO.setText(Integer.toString(calculoPeso));
                     break;
-                case 1:
-                    // ANCLA
-                    validarTipoYValor(0, 1);
-                    switch (cmb_TIPO.getSelectedItemPosition()){
-                        case 0:
-                            // OR
-                            break;
-                        case 1:
-                            // Baño de Oro
-                            break;
-                        case 2:
-                            // Oro Rosado
-                            break;
-                        case 3:
-                            // Plata
-                            break;
-                        case 4:
-                            // Níquel
-                            break;
-                    }
+                case  1:
+                    calculoPeso = Integer.parseInt(campo);
+                    calculoPeso = calculoPeso * 3200;
+                    VALOR.setText(Integer.toString(calculoPeso));
+                    calculoPeso = calculoPeso * cantidad;
+                    RESULTADO.setText("");
+                    RESULTADO.setText(Integer.toString(calculoPeso));
                     break;
-                }
-                break;
-            case 1:
-                // CUERDA
-                switch (cmb_DIJE.getSelectedItemPosition()){
-                    case 0:
-                        // MARTILLO
-                        validarTipoYValor(1, 0);
-                        switch (cmb_TIPO.getSelectedItemPosition()){
-                            case 0:
-                                // OR
-                                break;
-                            case 1:
-                                // Baño de Oro
-                                break;
-                            case 2:
-                                // Oro Rosado
-                                break;
-                            case 3:
-                                // Plata
-                                break;
-                            case 4:
-                                // Níquel
-                                break;
-                        }
-                        break;
-                    case 1:
-                        // ANCLA
-                        validarTipoYValor(1, 1);
-                        switch (cmb_TIPO.getSelectedItemPosition()){
-                            case 0:
-                                // OR
-                                break;
-                            case 1:
-                                // Baño de Oro
-                                break;
-                            case 2:
-                                // Oro Rosado
-                                break;
-                            case 3:
-                                // Plata
-                                break;
-                            case 4:
-                                // Níquel
-                                break;
-                        }
-                        break;
-                }
-                break;
+            }
         }
     }
 
+    public String buscarPrecios(String campo){
+        int f, c;
+        for (f = 0; f < ARRAY_PRECIOS.length; f++) {
+            for (c = 0; c < ARRAY_PRECIOS[f].length; c++) {
 
-    public void validarTipoYValor(int material, int dije){
-
+                if (ARRAY_PRECIOS[f][c] != null){
+                    if (ARRAY_PRECIOS[f][c].equals(campo)){
+                        return ARRAY_PRECIOS[f][c+1];
+                    }
+                }
+            }
+        }
+        return null;
     }
 
 
